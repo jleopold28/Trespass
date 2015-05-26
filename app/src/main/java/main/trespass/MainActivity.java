ccec962979752a99fdad4d3380a4294e5d0943ba
James Leopold <jleopold28@gmail.com>
2015-05-26 19:37:44 -0400

    test commit

--- app/src/main/java/main/trespass/MainActivity.java
+++ app/src/main/java/main/trespass/MainActivity.java
@@ -2,11 +2,12 @@ package main.trespass;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,6 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        //Test
         return true;
     }
 
