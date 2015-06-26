47387f308e0ad78bdc0a455721bc9748e6fdad6b
Yaxiong <yliu710@gatech.edu>
2015-06-26 14:04:26 -0400

    Add game logic but bug still exits BUG FOUND: when the selected tile is at the row 6 and want slide moves, it detects only one tile.
    Test class is writen for testing the game logic, will be deleted when BUGs fixed.

--- app/src/main/java/main/trespass/GameBoard.java
+++ app/src/main/java/main/trespass/GameBoard.java
@@ -1,16 +1,33 @@
 package main.trespass;
 
+import java.util.ArrayList;
+
 /**
  * Created by jleopold on 6/24/2015.
  */
 public class GameBoard {
     private Tile gameBoard[][];
+    private int[][][] boardPos; // a 3d array to record the position of each spot on the board
 
     private final int rowCount = 6;
     private final int colCount = 5;
 
+    private ArrayList<int[]> validTiles = new ArrayList<>();
+
     public GameBoard(){
         gameBoard = new Tile[rowCount][colCount];
+        for(int r = 0; r<rowCount;r++) { //initialize the board
+            for(int c = 0; c<colCount;c++) {
+                gameBoard[r][c] = new Tile();
+            }
+        }
+        boardPos = new int[rowCount][colCount][2]; //create the position array
+        for(int r = 0; r<rowCount;r++) {
+            for(int c = 0; c<colCount;c++) {
+                boardPos[r][c][0]=r;
+                boardPos[r][c][1]=c;
+            }
+        }
     }
     public Tile getTile( int rowCount, int colCount){
         return gameBoard[rowCount][colCount];
@@ -40,6 +57,68 @@ public class GameBoard {
         //return 1 for player win
         //return 2 for opponent win
         return 0;
+    }
+
+    /*
+    ** Get the valid tiles
+    ** args row  the row of the tile that player select
+    ** args col  the column of the tile that player select
+    *
+    * return  An ArrayList of arrays, each array has two indices, the first index is the row of the
+    *         valid tile, and the second index is the column of the valid tile
+     */
+    public ArrayList<int[]> getValidTiles(int row, int col) {
+        validTiles.clear();
+        forwardValidation(row,col); //get the valid tiles when moving forward
+        slideValidation(row,col); //get the valid tiles when moving left or right
+        jumpValidation(row,col); //get the valid tiles when jumping
+        return validTiles;
+    }
+    private void forwardValidation(int row, int col) {
+        if (row>0) {
+            if ((gameBoard[row-1][col].isBlank()) && (!validTiles.contains(boardPos[row-1][col])) ) { //check if the front tile is blank
+                validTiles.add(boardPos[row-1][col]); //the front tile is valid
+                forwardValidation(row-1,col); //get the forward moving valid tiles after moving forward
+                slideValidation(row-1,col); // get the slide moving valid tiles after moving forward
+                jumpValidation(row-1,col); // get the jumping valid tiles after moving forward
+            }
+        }
+    }
+    private void slideValidation(int row, int col) {
+        if (col>0) {
+            if ((gameBoard[row][col-1].isBlank()) && (!validTiles.contains(boardPos[row][col-1]))) { //check if the left tile is blank
+                validTiles.add(boardPos[row][col-1]); //the left tile is valid
+                slideValidation(row,col-1); //get the slide moving valid tiles after moving to the left
+                jumpValidation(row,col-1); //get the jumping valid tiles after moving to the left
+            }
+        }
+        if (col<colCount-1) {
+            if ((gameBoard[row][col+1].isBlank()) && (!validTiles.contains(boardPos[row][col+1]))) { //check if the right tile is blank
+                validTiles.add(boardPos[row][col+1]); //the right tile is valid
+                slideValidation(row,col+1); //get the slide moving valid tiles after moving to the right
+                jumpValidation(row,col+1); //get the jumping valid tiles after moving to the right
+            }
+        }
+    }
+    private void jumpValidation(int row, int col) {
+        if (row>1) {
+            if ((!gameBoard[row-1][col].isBlank()) && (gameBoard[row-2][col].isBlank()) && (!validTiles.contains(boardPos[row-2][col]))) { //check if it could jump forward
+                validTiles.add(boardPos[row-2][col]); //the forward jumping is valid
+                jumpValidation(row-2,col); //get the jumping valid tiles after jumping forward
+            }
+        }
+        if (col>1) {
+            if ((!gameBoard[row][col-1].isBlank()) && (gameBoard[row][col-2].isBlank()) && (!validTiles.contains(boardPos[row][col-2]))) { //check if it could jump to the left
+                validTiles.add(boardPos[row][col-2]); //the left jumping is valid
+                jumpValidation(row, col-2); //get the jumping valid tiles after jumping left
+            }
+        }
+        if (col<colCount-2) {
+            if ((!gameBoard[row][col+1].isBlank()) && (gameBoard[row][col+2].isBlank()) && (!validTiles.contains(boardPos[row][col+2]))) { //check if it could jump to the right
+                validTiles.add(boardPos[row][col+2]); //the right jumping is valid
+                jumpValidation(row,col+2); //get the jumping valid tiles after jumping right
+            }
+        }
     }
 
 }
